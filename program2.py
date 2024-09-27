299aad3694cb2866491593ccac2034db0d0c7361
Rahul Singh <141874118+rahulsingh1406@users.noreply.github.com>
2024-09-27 15:20:25 +0530

    Update program2.py
    done!

--- program2.py
+++ program2.py
@@ -1,10 +1,51 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        translations = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+        number = 0
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+        for char in s:
+            number += translations[char]
+        return number
 
+import unittest
+
+class TestRomanToInt(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+
+    def test_example1(self):
+        self.assertEqual(self.solution.romanToInt("III"), 3)
+
+    def test_example2(self):
+        self.assertEqual(self.solution.romanToInt("LVIII"), 58)
+
+    def test_example3(self):
+        self.assertEqual(self.solution.romanToInt("MCMXCIV"), 1994)
+
+    def test_single_roman_digit(self):
+        self.assertEqual(self.solution.romanToInt("X"), 10)
+
+    def test_subtraction_rule(self):
+        self.assertEqual(self.solution.romanToInt("IV"), 4)
+        self.assertEqual(self.solution.romanToInt("IX"), 9)
+
+    def test_large_number(self):
+        self.assertEqual(self.solution.romanToInt("MMMCMXCIX"), 3999)
+
+    def test_empty_string(self):
+        self.assertEqual(self.solution.romanToInt(""), 0)
+
+if _name_ == '_main_':
+    unittest.main(argv=['first-arg-is-ignored'], exit=False)
 
 
